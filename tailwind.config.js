92e3974c3b4207f4d8d0c49770f821872ea31bf5
EfrenOR <efren.or@outlook.com>
2022-07-15 20:58:52 -0500

    Se finalizoól pagáa index.html y se creo la página para el crud personas

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js,css}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['nunito', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
